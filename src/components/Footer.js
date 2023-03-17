41312916b6b28c8132c4537dcdfd227c2f68b995
Rizavia Sayeed <rizavia.sayeed09@gmail.com>
2023-03-17 22:27:19 +0530

    commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="bg-black text-neutral-300 mt-16 py-10">
-      <h3 className="text-center">This will be our Footer</h3>
+      <h1 className="text-center">This will be our Footer</h1>
       <div className="flex justify-between mx-20">
         <div>
           <h2>
@@ -11,6 +11,15 @@ const Footer = () => {
           <h3>Careers</h3>
           <h3>Blog</h3>
           <h3>Bug Bounty</h3>
+        </div>
+        <div>
+          <h2>
+            <u>Legal</u>
+          </h2>
+          <h3 className="mt-5">Terms & Conditions</h3>
+          <h3>Refund Policy</h3>
+          <h3>Cancellation criterion</h3>
+          <h3>Offer Terms</h3>
         </div>
         <div>
           <h2>
